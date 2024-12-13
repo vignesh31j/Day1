74b495661864756f4696f290b5adae4e2254eacb
Tarun <tarun@gmail.com>
2024-12-13 20:27:08 +0530

    commit

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ let testimonial_data =[
     {
         name:"Jaya Vignesh",
         image:"https://picsum.photos/200/200?random=2",
-        text_content:"I am very happy with the service provided by the company. They are very professional and nice"
+        text_content:"This company surpasses its competitors in service quality. Their products are of high quality, delivered promptly. I highly recommend this company."
     },
     {
         name:"Sharath Kumar",  
         image:"https://picsum.photos/200/200?random=3",
-        text_content:"I am very happy with the service provided by the company. They are very professional and super"
+        text_content:"I needed a refund for tickets to an event that was changed last minute. I experienced great customer service and the issue was resolved in a timely manner."
     },
     {
         name:"Vignesh",
@@ -17,7 +17,7 @@ let testimonial_data =[
     {
         name:"Sharath",
         image:"https://picsum.photos/200/200?random=5",
-        text_content:"I am very happy with the service provided by the company. They are very professional and better"
+        text_content:"I am completely blown away by how effective Company Name's digital marketing is Thighly recommend this to any business of any size."
     }
     
         
